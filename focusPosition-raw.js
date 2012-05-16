163a96310e522cc95b96667922deace96eb3426f
Prajwalit Bhopale <prajwalit@gmail.com>
2012-05-16 17:13:08 +0530

    Some modifications to focusPosition code.

--- focusPosition-raw.js
+++ focusPosition-raw.js
@@ -14,31 +14,29 @@
 YUI.add("focusPosition", function(Y) {
   var _focus = Y.Node.prototype.focus;
 
-  //Y.Node.prototype._focus = _focus;
-  Y.Node.prototype.focus = function (pos) {
-    if (!pos) {
+  Y.Node.prototype.focus = function (position) {
+
+    Y.log (position);
+    if (position === "end") {
+      position = this.get ("value").length;
+    } else if (!Y.Lang.isNumber (position)) {
       _focus.apply (this);
       return;
-    }
-    if (pos === "end") {
-      pos = this.get ("value").length;
     }
 
     var _node = this._node;
     /* Code Taken From ->
-       http://stackoverflow.com/questions/512528/set-cursor-position-in-html-textbox
-*/
+       http://stackoverflow.com/questions/512528/set-cursor-position-in-html-textbox */
     if(_node.setSelectionRange) {
       _node.focus ();
-      _node.setSelectionRange (pos, pos);
+      _node.setSelectionRange (position, position);
     } else if (_node.createTextRange) {
       var range = _node.createTextRange();
       range.collapse (true);
-      range.moveEnd ('character', pos);
-      range.moveStart ('character', pos);
+      range.moveEnd ('character', position);
+      range.moveStart ('character', position);
       range.select ();
     }
-    
-  }
+  };
   
 }, "0.0.1", {requires:["node"]});
